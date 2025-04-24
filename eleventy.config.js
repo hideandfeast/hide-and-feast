a154cc31b80602eb15a3f7937e0489274c2da87e
hideandfeast <hideandfeast@gmail.com>
2025-04-24 11:55:05 -0500

    Simplify config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,14 +1,4 @@
 export default function (eleventyConfig) {
-  // Copy the css folder to the output
   eleventyConfig.addPassthroughCopy("css");
-
-  return {
-    dir: {
-      input: "content",
-      includes: "../_includes",
-      layouts: "../_includes/layouts",
-      data: "../_data",
-      output: "_site",
-    },
-  };
+  return { dir: { output: "_site" } };
 }
