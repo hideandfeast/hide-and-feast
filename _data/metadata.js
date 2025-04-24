4c3b899c1d34c6b3f0039ab1cc371eccedac20a2
hideandfeast <hideandfeast@gmail.com>
2025-04-24 10:20:26 -0500

    Fix metadata.js syntax

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,10 +1,10 @@
-{
-  "title": "Hide & Feast",
-  "url": "https://hideandfeast.com",
-  "description": "Tastefully Curated Travel – Value Meets Soul",
-  "author": {
-    "name": "Hide & Feast",
-    "url": "https://hideandfeast.com/about/"
+module.exports = {
+  title: "Hide & Feast",
+  url: "https://hideandfeast.com",
+  description: "Tastefully Curated Travel – Value Meets Soul",
+  author: {
+    name: "Hide & Feast",
+    url: "https://hideandfeast.com/about/"
   },
-  "locale": "en_US"
-}
+  locale: "en_US"
+};
